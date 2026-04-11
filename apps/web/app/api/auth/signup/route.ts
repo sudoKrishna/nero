617d08ab2ce0f8dbd5c18ea675d1835633dcbdbe
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2026-04-12 03:03:25 +0530

    update login and sigup page

--- apps/web/app/api/auth/signup/route.ts
+++ apps/web/app/api/auth/signup/route.ts
@@ -1,6 +1,6 @@
 import { prisma } from "@nero/db";
 import bcrypt from "bcryptjs";
-import { error } from "console";
+
 
 export async function POST(req : Request) {
     const body = await req.json();
